aafa96deac7e1bb102b38f0fa4cede273ec957a9
hugovaldez <hugovaldez@email.arizona.edu>
2016-11-03 11:33:58 -0700

    getters & setters for Card

--- src/World/Card.java
+++ src/World/Card.java
@@ -1,5 +1,26 @@
 package World;
-
+import Player.Player;
 public class Card {
-
+	private Country country;
+	private int numStars;
+	private Player player;
+	
+	public void setCountry(Country c){
+		country = c;
+	}
+	public Country getCountry(){
+		return country;
+	}
+	public void setNumStars(int stars){
+		numStars = stars;
+	}
+	public int getNumStars(){
+		return numStars;
+	}
+	public void setPlayer(Player p){
+		player = p;
+	}
+	public Player getPlayer(){
+		return player;
+	}
 }
