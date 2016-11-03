6631524c3989cd9a14347d255aee4af8e39a20b6
hugovaldez <hugovaldez@email.arizona.edu>
2016-11-03 10:40:39 -0700

    changed bye

--- src/driver.java
+++ src/driver.java
@@ -3,7 +3,7 @@ public class driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Bye");
+		System.out.println("Suck Me Mr. 104");
 
 	}
 
