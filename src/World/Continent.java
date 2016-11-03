017f5fcffd11ef52c92d2cd059a72e841f211b2f
hugovaldez <hugovaldez@email.arizona.edu>
2016-11-03 11:57:25 -0700

    skeleton of continent

--- src/World/Continent.java
+++ src/World/Continent.java
@@ -1,5 +1,57 @@
 package World;
 
-public class Continent {
+import java.util.ArrayList;
 
+import Player.Player;
+
+public class Continent {
+	private ArrayList<Country> countries;
+	private int bonus;
+	private String color;
+	private String name;
+	private World world;
+	
+	public void addCountry(Country c){
+		countries.add(c);
+	}
+	public void removeCountry(Country c){
+		int i = 0;
+		for(Country A : countries){
+			if(A.equals(c)){
+				countries.remove(A);
+				break;
+			}
+		}
+	}
+	public ArrayList<Country> getCountries(){
+		return countries;
+	}
+	public void setBonus(int bonus){
+		this.bonus = bonus;
+	}
+	public int getBonus(){
+		return bonus;
+	}
+	public void setColor(String col){
+		color = col;
+	}
+	public String getColor(){
+		return color;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setWorld(World w){
+		world = w;
+	}
+	public World getWorld(){
+		return world;
+	}
+	public Player checkIfRuled(){
+		Player placeHolder = new Player();
+		return placeHolder;
+	}
 }
