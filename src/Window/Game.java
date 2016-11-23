41d879afa3c4c6f16b45b798d3f8e9d972419d12
hugovaldez <hugovaldez@email.arizona.edu>
2016-11-23 10:06:29 -0700

    buttons work

--- src/Window/Game.java
+++ src/Window/Game.java
@@ -37,9 +37,19 @@ public class Game {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					JFrame frame1 = new JFrame();
+					frame1.setResizable(false);
+					frame1.setTitle("Risk - The Game of Global Domination");
+					frame1.getContentPane().setBackground(Color.LIGHT_GRAY);
+					frame1.setBounds(100, 100, 1200, 900);
+					frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					frame1.setVisible(true);
+					frame1.getContentPane().setLayout(new CardLayout(0, 0));
+					
 					JPanel thing = new JPanel();
+					thing.setLayout(null);
 					Game window = new Game(thing);
-					window.frame.setVisible(true);
+					frame1.getContentPane().add(thing);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -120,12 +130,15 @@ public class Game {
 		CountryButton WesternUS = new CountryButton(ImageIO.read(new File("GimpFiles\\Countries\\WesternUnitedStates.png")), "Western US");
 		CountryButton Yakutsk = new CountryButton(ImageIO.read(new File("GimpFiles\\Countries\\Yakutsk.png")), "Yakutsk");
 		
+		int x = 9;
+		int y = 165;
+		
 		JButton btnNewButton = new JButton((String) null);
 		btnNewButton.setOpaque(false);
 		btnNewButton.setBorderPainted(false);
 		btnNewButton.setContentAreaFilled(false);
 		btnNewButton.setIcon(new ImageIcon("GimpFiles\\Countries\\Alaska.png"));
-		Alaska.b.setBounds(7, 177, 101, 100);
+		Alaska.b.setBounds(x, y, 101, 100);
 		frame.add(Alaska.b);
 		
 		JButton btnNewButton_1 = new JButton((String) null);
@@ -133,7 +146,7 @@ public class Game {
 		btnNewButton_1.setOpaque(false);
 		btnNewButton_1.setContentAreaFilled(false);
 		btnNewButton_1.setBorderPainted(false);
-		NWTerritory.b.setBounds(82, 159, 175, 83);
+		NWTerritory.b.setBounds(x + 75, y - 18, 175, 83);
 		frame.add(NWTerritory.b);
 		
 		JButton btnNewButton_2 = new JButton((String) null);
@@ -141,7 +154,7 @@ public class Game {
 		btnNewButton_2.setContentAreaFilled(false);
 		btnNewButton_2.setBorderPainted(false);
 		btnNewButton_2.setIcon(new ImageIcon("GimpFiles\\Countries\\Quebec.png"));
-		Quebec.b.setBounds(238, 227, 87, 118);
+		Quebec.b.setBounds(x + 231, y + 50, 87, 118);
 		frame.add(Quebec.b);
 		
 		JButton btnNewButton_3 = new JButton((String) null);
@@ -149,7 +162,7 @@ public class Game {
 		btnNewButton_3.setContentAreaFilled(false);
 		btnNewButton_3.setOpaque(false);
 		btnNewButton_3.setIcon(new ImageIcon("GimpFiles\\Countries\\Alberta.png"));
-		Alberta.b.setBounds(90, 235, 96, 71);
+		Alberta.b.setBounds(x + 83, y + 58, 96, 71);
 		frame.add(Alberta.b);
 		
 		JButton btnNewButton_4 = new JButton((String) null);
@@ -157,14 +170,14 @@ public class Game {
 		btnNewButton_4.setBorderPainted(false);
 		btnNewButton_4.setContentAreaFilled(false);
 		btnNewButton_4.setIcon(new ImageIcon("GimpFiles\\Countries\\Ontario.png"));
-		Ontario.b.setBounds(173, 234, 75, 90);
+		Ontario.b.setBounds(x + 166, y + 57, 75, 90);
 		frame.add(Ontario.b);
 		
 		JButton btnNewButton_5 = new JButton((String) null);
 		btnNewButton_5.setIcon(new ImageIcon("GimpFiles\\Countries\\EasternUnitedStates.png"));
 		btnNewButton_5.setContentAreaFilled(false);
 		btnNewButton_5.setBorderPainted(false);
-		EasternUS.b.setBounds(158, 302, 127, 118);
+		EasternUS.b.setBounds(x + 151, y + 125, 127, 118);
 		frame.add(EasternUS.b);
 		
 		JButton btnNewButton_6 = new JButton((String) null);
@@ -172,7 +185,7 @@ public class Game {
 		btnNewButton_6.setBorderPainted(false);
 		btnNewButton_6.setContentAreaFilled(false);
 		btnNewButton_6.setIcon(new ImageIcon("GimpFiles\\Countries\\WesternUnitedStates.png"));
-		WesternUS.b.setBounds(97, 300, 101, 100);
+		WesternUS.b.setBounds(x + 90, y + 123, 101, 100);
 		frame.add(WesternUS.b);
 		
 		JButton btnNewButton_7 = new JButton((String) null);
@@ -180,7 +193,7 @@ public class Game {
 		btnNewButton_7.setContentAreaFilled(false);
 		btnNewButton_7.setOpaque(false);
 		btnNewButton_7.setIcon(new ImageIcon("GimpFiles\\Countries\\CenAm.png"));
-		CenAm.b.setBounds(100, 374, 107, 118);
+		CenAm.b.setBounds(x + 93, y + 197, 107, 118);
 		frame.add(CenAm.b);
 		
 		JButton btnNewButton_8 = new JButton((String) null);
@@ -188,7 +201,7 @@ public class Game {
 		btnNewButton_8.setContentAreaFilled(false);
 		btnNewButton_8.setOpaque(false);
 		btnNewButton_8.setIcon(new ImageIcon("GimpFiles\\Countries\\Venezuela.png"));
-		Venezuela.b.setBounds(177, 455, 127, 71);
+		Venezuela.b.setBounds(x + 170, y + 278, 127, 71);
 		frame.add(Venezuela.b);
 		
 		JButton btnNewButton_9 = new JButton((String) null);
@@ -196,7 +209,7 @@ public class Game {
 		btnNewButton_9.setContentAreaFilled(false);
 		btnNewButton_9.setBorderPainted(false);
 		btnNewButton_9.setIcon(new ImageIcon("GimpFiles\\Countries\\Argentina.png"));
-		Argentina.b.setBounds(209, 585, 94, 175);
+		Argentina.b.setBounds(x + 202, y + 408, 94, 175);
 		frame.add(Argentina.b);
 		
 		JButton btnNewButton_10 = new JButton((String) null);
@@ -204,7 +217,7 @@ public class Game {
 		btnNewButton_10.setBorderPainted(false);
 		btnNewButton_10.setContentAreaFilled(false);
 		btnNewButton_10.setIcon(new ImageIcon("GimpFiles\\Countries\\Brazil.png"));
-		Brazil.b.setBounds(198, 487, 188, 168);
+		Brazil.b.setBounds(x + 191, y + 310, 188, 168);
 		frame.add(Brazil.b);
 		
 		JButton btnNewButton_11 = new JButton((String) null);
@@ -212,7 +225,7 @@ public class Game {
 		btnNewButton_11.setBorderPainted(false);
 		btnNewButton_11.setContentAreaFilled(false);
 		btnNewButton_11.setIcon(new ImageIcon("GimpFiles\\Countries\\Peru.png"));
-		Peru.b.setBounds(165, 499, 128, 128);
+		Peru.b.setBounds(x + 158, y + 322, 128, 128);
 		frame.add(Peru.b);
 		
 		JButton btnNewButton_12 = new JButton((String) null);
@@ -220,7 +233,7 @@ public class Game {
 		btnNewButton_12.setBorderPainted(false);
 		btnNewButton_12.setContentAreaFilled(false);
 		btnNewButton_12.setIcon(new ImageIcon("GimpFiles\\Countries\\NorthAfrica.png"));
-		NorthAfrica.b.setBounds(378, 446, 168, 163);
+		NorthAfrica.b.setBounds(x + 371, y + 269, 168, 163);
 		frame.add(NorthAfrica.b);
 		
 		JButton btnNewButton_13 = new JButton((String) null);
@@ -228,7 +241,7 @@ public class Game {
 		btnNewButton_13.setContentAreaFilled(false);
 		btnNewButton_13.setOpaque(false);
 		btnNewButton_13.setIcon(new ImageIcon("GimpFiles\\Countries\\Congo.png"));
-		Congo.b.setBounds(488, 561, 101, 100);
+		Congo.b.setBounds(x + 481, y + 384, 101, 100);
 		frame.add(Congo.b);
 		
 		JButton btnNewButton_14 = new JButton((String) null);
@@ -236,7 +249,7 @@ public class Game {
 		btnNewButton_14.setBorderPainted(false);
 		btnNewButton_14.setContentAreaFilled(false);
 		btnNewButton_14.setIcon(new ImageIcon("GimpFiles\\Countries\\SouthAfrica.png"));
-		SouthAfrica.b.setBounds(498, 627, 116, 132);
+		SouthAfrica.b.setBounds(x + 491, y + 450, 116, 132);
 		frame.add(SouthAfrica.b);
 		
 		JButton btnNewButton_15 = new JButton((String) null);
@@ -244,7 +257,7 @@ public class Game {
 		btnNewButton_15.setBorderPainted(false);
 		btnNewButton_15.setContentAreaFilled(false);
 		btnNewButton_15.setIcon(new ImageIcon("GimpFiles\\Countries\\EastAfrica.png"));
-		EastAfrica.b.setBounds(537, 515, 116, 160);
+		EastAfrica.b.setBounds(x + 530, y + 338, 116, 160);
 		frame.add(EastAfrica.b);
 		
 		JButton btnNewButton_16 = new JButton((String) null);
@@ -252,7 +265,7 @@ public class Game {
 		btnNewButton_16.setBorderPainted(false);
 		btnNewButton_16.setContentAreaFilled(false);
 		btnNewButton_16.setIcon(new ImageIcon("GimpFiles\\Countries\\Egypt.png"));
-		Egypt.b.setBounds(492, 453, 100, 83);
+		Egypt.b.setBounds(x + 485, y + 276, 100, 83);
 		frame.add(Egypt.b);
 		
 		JButton btnNewButton_17 = new JButton((String) null);
@@ -260,14 +273,14 @@ public class Game {
 		btnNewButton_17.setBorderPainted(false);
 		btnNewButton_17.setContentAreaFilled(false);
 		btnNewButton_17.setIcon(new ImageIcon("GimpFiles\\Countries\\Madagascar.png"));
-		Madagascar.b.setBounds(592, 632, 78, 110);
+		Madagascar.b.setBounds(x + 585, y + 455, 78, 110);
 		frame.add(Madagascar.b);
 		
 		JButton btnNewButton_18 = new JButton((String) null);
 		btnNewButton_18.setBorderPainted(false);
 		btnNewButton_18.setContentAreaFilled(false);
 		btnNewButton_18.setIcon(new ImageIcon("GimpFiles\\Countries\\Greenland.png"));
-		Greenland.b.setBounds(207, 127, 198, 133);
+		Greenland.b.setBounds(x + 200, y - 50, 198, 133);
 		frame.add(Greenland.b);
 		
 		JButton btnNewButton_19 = new JButton((String) null);
@@ -275,7 +288,7 @@ public class Game {
 		btnNewButton_19.setBorderPainted(false);
 		btnNewButton_19.setContentAreaFilled(false);
 		btnNewButton_19.setIcon(new ImageIcon("GimpFiles\\Countries\\Iceland.png"));
-		Iceland.b.setBounds(375, 219, 87, 81);
+		Iceland.b.setBounds(x + 368, y + 42, 87, 81);
 		frame.add(Iceland.b);
 		
 		JButton btnNewButton_20 = new JButton((String) null);
@@ -283,7 +296,7 @@ public class Game {
 		btnNewButton_20.setContentAreaFilled(false);
 		btnNewButton_20.setBorderPainted(false);
 		btnNewButton_20.setIcon(new ImageIcon("GimpFiles\\Countries\\Britain.png"));
-		Britain.b.setBounds(346, 269, 116, 96);
+		Britain.b.setBounds(x + 339, y + 92, 116, 96);
 		frame.add(Britain.b);
 		
 		JButton btnNewButton_21 = new JButton((String) null);
@@ -291,7 +304,7 @@ public class Game {
 		btnNewButton_21.setContentAreaFilled(false);
 		btnNewButton_21.setBorderPainted(false);
 		btnNewButton_21.setIcon(new ImageIcon("GimpFiles\\Countries\\Scandinavia.png"));
-		Scandinavia.b.setBounds(436, 189, 120, 112);
+		Scandinavia.b.setBounds(x + 429, y + 12, 120, 112);
 		frame.add(Scandinavia.b);
 		
 		JButton btnNewButton_22 = new JButton((String) null);
@@ -299,7 +312,7 @@ public class Game {
 		btnNewButton_22.setContentAreaFilled(false);
 		btnNewButton_22.setBorderPainted(false);
 		btnNewButton_22.setIcon(new ImageIcon("GimpFiles\\Countries\\Ukraine.png"));
-		Ukraine.b.setBounds(519, 189, 158, 224);
+		Ukraine.b.setBounds(x + 512, y + 12, 158, 224);
 		frame.add(Ukraine.b);
 		
 		JButton btnNewButton_23 = new JButton((String) null);
@@ -307,7 +320,7 @@ public class Game {
 		btnNewButton_23.setContentAreaFilled(false);
 		btnNewButton_23.setBorderPainted(false);
 		btnNewButton_23.setIcon(new ImageIcon("GimpFiles\\Countries\\WesternEurope.png"));
-		WesternEurope.b.setBounds(380, 349, 90, 116);
+		WesternEurope.b.setBounds(x + 373, y + 172, 90, 116);
 		frame.add(WesternEurope.b);
 		
 		JButton btnNewButton_24 = new JButton((String) null);
@@ -315,7 +328,7 @@ public class Game {
 		btnNewButton_24.setContentAreaFilled(false);
 		btnNewButton_24.setBorderPainted(false);
 		btnNewButton_24.setIcon(new ImageIcon("GimpFiles\\Countries\\NorthernEurope.png"));
-		NorthernEurope.b.setBounds(431, 284, 118, 94);
+		NorthernEurope.b.setBounds(x + 424, y + 107, 118, 94);
 		frame.add(NorthernEurope.b);
 		
 		JButton btnNewButton_25 = new JButton((String) null);
@@ -323,7 +336,7 @@ public class Game {
 		btnNewButton_25.setBorderPainted(false);
 		btnNewButton_25.setContentAreaFilled(false);
 		btnNewButton_25.setIcon(new ImageIcon("GimpFiles\\Countries\\SouthernEurope.png"));
-		SouthernEurope.b.setBounds(456, 352, 100, 114);
+		SouthernEurope.b.setBounds(x + 449, y + 175, 100, 114);
 		frame.add(SouthernEurope.b);
 		
 		JButton btnNewButton_26 = new JButton((String) null);
@@ -331,7 +344,7 @@ public class Game {
 		btnNewButton_26.setContentAreaFilled(false);
 		btnNewButton_26.setBorderPainted(false);
 		btnNewButton_26.setIcon(new ImageIcon("GimpFiles\\Countries\\MiddleEast.png"));
-		MiddleEast.b.setBounds(534, 399, 160, 153);
+		MiddleEast.b.setBounds(x + 527, y + 222, 160, 153);
 		frame.add(MiddleEast.b);
 		
 		JButton btnNewButton_27 = new JButton((String) null);
@@ -339,7 +352,7 @@ public class Game {
 		btnNewButton_27.setContentAreaFilled(false);
 		btnNewButton_27.setOpaque(false);
 		btnNewButton_27.setIcon(new ImageIcon("GimpFiles\\Countries\\Afghanistan.png"));
-		Afghanistan.b.setBounds(614, 298, 121, 120);
+		Afghanistan.b.setBounds(x + 607, y + 121, 121, 120);
 		frame.add(Afghanistan.b);
 		
 		JButton btnNewButton_28 = new JButton((String) null);
@@ -347,7 +360,7 @@ public class Game {
 		btnNewButton_28.setContentAreaFilled(false);
 		btnNewButton_28.setBorderPainted(false);
 		btnNewButton_28.setIcon(new ImageIcon("GimpFiles\\Countries\\India.png"));
-		India.b.setBounds(673, 391, 122, 172);
+		India.b.setBounds(x + 666, y + 214, 122, 172);
 		frame.add(India.b);
 		
 		JButton btnNewButton_29 = new JButton((String) null);
@@ -355,7 +368,7 @@ public class Game {
 		btnNewButton_29.setBorderPainted(false);
 		btnNewButton_29.setOpaque(false);
 		btnNewButton_29.setIcon(new ImageIcon("GimpFiles\\Countries\\Siam.png"));
-		Siam.b.setBounds(777, 445, 82, 104);
+		Siam.b.setBounds(x + 770, y + 268, 82, 104);
 		frame.add(Siam.b);
 		
 		JButton btnNewButton_30 = new JButton((String) null);
@@ -363,7 +376,7 @@ public class Game {
 		btnNewButton_30.setBorderPainted(false);
 		btnNewButton_30.setContentAreaFilled(false);
 		btnNewButton_30.setIcon(new ImageIcon("GimpFiles\\Countries\\China.png"));
-		China.b.setBounds(712, 321, 172, 154);
+		China.b.setBounds(x + 705, y + 144, 172, 154);
 		frame.add(China.b);
 		
 		JButton btnNewButton_31 = new JButton((String) null);
@@ -371,7 +384,7 @@ public class Game {
 		btnNewButton_31.setOpaque(false);
 		btnNewButton_31.setContentAreaFilled(false);
 		btnNewButton_31.setIcon(new ImageIcon("GimpFiles\\Countries\\Mongolia.png"));
-		Mongolia.b.setBounds(771, 288, 144, 96);
+		Mongolia.b.setBounds(x + 764, y + 111, 144, 96);
 		frame.add(Mongolia.b);
 		
 		JButton btnNewButton_32 = new JButton((String) null);
@@ -379,7 +392,7 @@ public class Game {
 		btnNewButton_32.setContentAreaFilled(false);
 		btnNewButton_32.setBorderPainted(false);
 		btnNewButton_32.setIcon(new ImageIcon("GimpFiles\\Countries\\Ural.png"));
-		Ural.b.setBounds(660, 166, 86, 178);
+		Ural.b.setBounds(x + 653, y - 11, 86, 178);
 		frame.add(Ural.b);
 		
 		JButton btnNewButton_33 = new JButton((String) null);
@@ -387,7 +400,7 @@ public class Game {
 		btnNewButton_33.setContentAreaFilled(false);
 		btnNewButton_33.setBorderPainted(false);
 		btnNewButton_33.setIcon(new ImageIcon("GimpFiles\\Countries\\Siberia.png"));
-		Siberia.b.setBounds(683, 133, 124, 210);
+		Siberia.b.setBounds(x + 676, y - 44, 124, 210);
 		frame.add(Siberia.b);
 		
 		JButton btnNewButton_34 = new JButton((String) null);
@@ -395,7 +408,7 @@ public class Game {
 		btnNewButton_34.setContentAreaFilled(false);
 		btnNewButton_34.setBorderPainted(false);
 		btnNewButton_34.setIcon(new ImageIcon("GimpFiles\\Countries\\Irkutsk.png"));
-		Irkutsk.b.setBounds(763, 231, 112, 86);
+		Irkutsk.b.setBounds(x + 756, y + 54, 112, 86);
 		frame.add(Irkutsk.b);
 		
 		JButton btnNewButton_35 = new JButton((String) null);
@@ -403,7 +416,7 @@ public class Game {
 		btnNewButton_35.setContentAreaFilled(false);
 		btnNewButton_35.setBorderPainted(false);
 		btnNewButton_35.setIcon(new ImageIcon("GimpFiles\\Countries\\Yakutsk.png"));
-		Yakutsk.b.setBounds(779, 151, 104, 94);
+		Yakutsk.b.setBounds(x + 772, y - 26, 104, 94);
 		frame.add(Yakutsk.b);
 		
 		JButton btnNewButton_36 = new JButton((String) null);
@@ -411,7 +424,7 @@ public class Game {
 		btnNewButton_36.setContentAreaFilled(false);
 		btnNewButton_36.setBorderPainted(false);
 		btnNewButton_36.setIcon(new ImageIcon("GimpFiles\\Countries\\Kamchatka.png"));
-		Kamchatka.b.setBounds(836, 163, 302, 172);
+		Kamchatka.b.setBounds(x + 829, y - 14, 302, 172);
 		frame.add(Kamchatka.b);
 		
 		JButton btnNewButton_37 = new JButton((String) null);
@@ -419,7 +432,7 @@ public class Game {
 		btnNewButton_37.setContentAreaFilled(false);
 		btnNewButton_37.setOpaque(false);
 		btnNewButton_37.setIcon(new ImageIcon("GimpFiles\\Countries\\Japan.png"));
-		Japan.b.setBounds(890, 285, 68, 118);
+		Japan.b.setBounds(x + 883, y + 108, 68, 118);
 		frame.add(Japan.b);
 		
 		JButton btnNewButton_38 = new JButton((String) null);
@@ -427,7 +440,7 @@ public class Game {
 		btnNewButton_38.setContentAreaFilled(false);
 		btnNewButton_38.setBorderPainted(false);
 		btnNewButton_38.setIcon(new ImageIcon("GimpFiles\\Countries\\WesternAustralia.png"));
-		WesternAustralia.b.setBounds(823, 591, 122, 158);
+		WesternAustralia.b.setBounds(x + 816, y + 414, 122, 158);
 		frame.add(WesternAustralia.b);
 		
 		JButton btnNewButton_39 = new JButton((String) null);
@@ -435,7 +448,7 @@ public class Game {
 		btnNewButton_39.setContentAreaFilled(false);
 		btnNewButton_39.setOpaque(false);
 		btnNewButton_39.setIcon(new ImageIcon("GimpFiles\\Countries\\EasternAustralia.png"));
-		EasternAustralia.b.setBounds(897, 584, 104, 178);
+		EasternAustralia.b.setBounds(x + 890, y + 407, 104, 178);
 		frame.add(EasternAustralia.b);
 		
 		JButton btnNewButton_40 = new JButton((String) null);
@@ -443,7 +456,7 @@ public class Game {
 		btnNewButton_40.setBorderPainted(false);
 		btnNewButton_40.setContentAreaFilled(false);
 		btnNewButton_40.setIcon(new ImageIcon("GimpFiles\\Countries\\Indonesia.png"));
-		Indonesia.b.setBounds(770, 535, 116, 110);
+		Indonesia.b.setBounds(x + 763, y + 358, 116, 110);
 		frame.add(Indonesia.b);
 		
 		JButton btnNewButton_41 = new JButton((String) null);
@@ -451,7 +464,7 @@ public class Game {
 		btnNewButton_41.setContentAreaFilled(false);
 		btnNewButton_41.setBorderPainted(false);
 		btnNewButton_41.setIcon(new ImageIcon("GimpFiles\\Countries\\NewGuinea.png"));
-		NewGuinea.b.setBounds(866, 542, 98, 72);
+		NewGuinea.b.setBounds(x + 859, y + 365, 98, 72);
 		frame.add(NewGuinea.b);
 		
 		JLabel lblNewLabel = new JLabel((String) null);
